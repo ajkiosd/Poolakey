c05a26656657b95932c1f3f51af89d6eb169f769
ajkiosd <ajkiosd@gmail.com>
2021-11-08 16:42:47 +0330

    Update Const.kt

--- poolakey/src/main/java/ir/cafebazaar/poolakey/constant/Const.kt
+++ poolakey/src/main/java/ir/cafebazaar/poolakey/constant/Const.kt
@@ -1,5 +1,5 @@
 package ir.cafebazaar.poolakey.constant
 
 internal object Const {
-    const val BAZAAR_PACKAGE_NAME = "com.farsitel.bazaar"
+    const val BAZAAR_PACKAGE_NAME = "ir.mservices.market"
 }
