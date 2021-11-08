ed0a456b6799aafd9edbdc657c92199b38efe271
ajkiosd <ajkiosd@gmail.com>
2021-11-08 18:15:08 +0330

    Update DisconnectException.kt

--- poolakey/src/main/java/ir/cafebazaar/poolakey/exception/DisconnectException.kt
+++ poolakey/src/main/java/ir/cafebazaar/poolakey/exception/DisconnectException.kt
@@ -3,6 +3,6 @@ package ir.cafebazaar.poolakey.exception
 class DisconnectException : IllegalStateException() {
 
     override val message: String?
-        get() = "We can't communicate with Bazaar: Service is disconnected"
+        get() = "We can't communicate with Myket: Service is disconnected"
 
 }
